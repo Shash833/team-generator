0de5398f826f9077a562439d03acd8caa11786f4
Shash833 <58815647+Shash833@users.noreply.github.com>
2020-04-16 22:20:50 +1000

    add files and README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# team-generator
+# Team Profile Generator
+
+## Introduction: 
+
+This application was created to allow the User to create a team profile (including a manager, engineers and interns). The user can generate their team profile through a series of questions about their team which are answered through the command line interface. The provided details about the team members are then used to render a HTML page. 
+
+## Usage/Installation: 
+
+The user must first download Node.js on their computer to use the application. After the repository is cloned from Git-Hub and relevent dependencies are installed, the user can run the application with the command "node app.js" on their terminal to display a series of questions about their team. When the user has completed providing information, they can generate their team profile. 
+
+---
+## Preview: 
+
+### CLI prompts and sample answers:
+![sample](/assets/sample-answers.PNG)
+
+### Sample team profile
+![sample](/assets/sample-page.PNG)
---
+++ app.js
@@ -0,0 +1,141 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+//Empty array to store employee objects
+const employeeList = []
+
+//Prompt user with initial questions to provide employee details:
+function promptEmployee() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Employee name: ",
+        },
+        {
+            type: "input",
+            name: "ID",
+            message: "Employee ID: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Employee email: "
+        },
+        {
+            type: "checkbox",
+            name: "role",
+            message: "Employee Role: ",
+            choices: ["manager", "engineer", "intern"]
+        },
+    ])
+}
+
+//Prompt user with role specific questions:
+function promptManager() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Manager office number: "
+        }
+    ])
+}
+
+function promptEngineer() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "Git-Hub username: "
+        }
+    ])
+}
+
+function promptIntern() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "school",
+            message: "Intern's school: "
+        }
+    ])
+}
+
+
+//Prompt user with choices to continue with adding more employees or to create HTML page.
+function continuePrompt() {
+    return inquirer.prompt([{
+        type: "checkbox",
+        name: "choice",
+        message: "Do you want to add any more employees?",
+        choices: ["Yes", "No. Generate team page."]
+    }])
+}
+
+//Function to be called after employee object has been created to prompt user with how to continue: to either create new employee objects or to render HTML
+async function continueOptions() {
+    try {
+        const proceed = await continuePrompt()
+        //If user chooses yes, run employeeProfile() again to create new employee object
+        if (proceed.choice == "Yes") {
+            employeeProfile()
+        }
+        //If user chooses "no", create HTML file using array of employee objects
+        else {
+            const teamFile = await render(employeeList)
+            fs.writeFile(outputPath, teamFile, function (error) {
+                if (error) {
+                    throw error
+                }
+                else {
+                    console.log("Team page has been created")
+                }
+            })
+        }
+    }
+    catch (error) { console.log(error) }
+}
+
+
+//To call functions which prompt the user.
+//Prompt user with initial employee questions, then based on role, display role specific questions.
+//With user answers, create new objects for epmloyees using role specific classes.
+//After employee object is made and added to array, prompt on how to continue by calling continueOptions().
+async function employeeProfile() {
+    try {
+        const employee = await promptEmployee()
+        if (employee.role == "manager") {
+            const manager = await promptManager()
+            employee.name = new Manager(employee.name, employee.ID, employee.email, manager.officeNumber)
+            employeeList.push(employee.name)
+            continueOptions()
+        }
+        else if (employee.role == "engineer") {
+            const engineer = await promptEngineer()
+            employee.name = new Engineer(employee.name, employee.ID, employee.email, engineer.github)
+            employeeList.push(employee.name)
+            continueOptions()
+        }
+        else {
+            const intern = await promptIntern()
+            employee.name = new Intern(employee.name, employee.ID, employee.email, intern.school)
+            employeeList.push(employee.name)
+            continueOptions()
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+employeeProfile()
